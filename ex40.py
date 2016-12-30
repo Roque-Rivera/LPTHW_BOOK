e78903d73aee32b01cccae233ff5c8b30cf687df
roquerivera <roque.rivera@gmail.com>
2016-12-29 18:52:23 -0600

    Continuando con el libro rama DEV.

---
+++ ex40.py
@@ -0,0 +1,20 @@
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
+
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to be sued",
+                   "So I'll stop right there"])
+
+bull_on_parade = Song(["The rally around tha family",
+                       "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bull_on_parade.sing_me_a_song()
+
